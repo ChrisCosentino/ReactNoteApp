243bb120b703c56e7e9562e34cc506dd91e1940c
Chris Cosentino <chriscosentino@hotmail.com>
2020-01-05 23:13:54 -0500

    Updates

---
+++ precache-manifest.0a8e5e31dee91e474eba01114c0faa99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "109872e82d2c9d15f97adafbabf023a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1be3b81e383ef8267e60",
+    "url": "/static/css/main.60d5545d.chunk.css"
+  },
+  {
+    "revision": "591be329406be5504c81",
+    "url": "/static/js/2.235dd9ad.chunk.js"
+  },
+  {
+    "revision": "2a196d222152c45027e5b2ccfe72e93e",
+    "url": "/static/js/2.235dd9ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1be3b81e383ef8267e60",
+    "url": "/static/js/main.4dbf7ca5.chunk.js"
+  },
+  {
+    "revision": "57a4011612fbbc393379",
+    "url": "/static/js/runtime-main.b91e6d17.js"
+  }
+]);
